c6b851396948fc09d4b8911aa98a33104fdfc9d3
Aldrin <gamaaldrin@gmail.com>
2020-06-28 13:14:14 +0200

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Aldrin Bahle Gama on 2020/06/28.
+//  Copyright © 2020 Aldrin Bahle Gama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
